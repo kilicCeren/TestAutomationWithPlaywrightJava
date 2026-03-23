01493a13c4f8dfd9385522c01ac17a96609e43c9
kilicCeren <kiliccrn07@gmail.com>
2026-03-23 13:52:25 +0300

    v8

---
+++ src/test/java/C09_PageSelect.java
@@ -0,0 +1,27 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class C09_PageSelect {
+
+    public static void main(String[] args) throws InterruptedException {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.setViewportSize(width,height);
+
+        page.navigate("https://testautomationpractice.blogspot.com/");
+        page.selectOption("//*[@id='country']","canada");
+        Thread.sleep(1000);
+
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
